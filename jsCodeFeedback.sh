a631e3166ae81df7e53a935b5b57ed0abd296a2b
Howard Chiam <hchiam@users.noreply.github.com>
2019-12-20 21:39:32 -0500

    Create jsCodeFeedback.sh

---
+++ jsCodeFeedback.sh
@@ -0,0 +1,31 @@
+# To run this file: bash jsCodeFeedback.sh
+
+function installEslintIfMissing() {
+  if ! [ -x "$(command -v eslint)" ]; then
+    echo 'Error: eslint is not set up or installed.'
+    npm install # this will install all the dependencies in package.json
+    # could specifically install these dependencies:
+    # npm install --save-dev eslint eslint-config-google eslint-plugin-html
+    # (you still need to a .eslintrc.js set up)
+  fi
+}
+
+function giveFeedback() {
+  echo
+  read -p "Do you want to continue and get feedback on templates/index.html? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    eslint templates/index.html
+  fi
+  echo
+  read -p "Do you want to continue and get feedback on templates/form.html? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    eslint templates/form.html
+  fi
+  echo
+  echo "Done."
+}
+
+installEslintIfMissing
+giveFeedback
