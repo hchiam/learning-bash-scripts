503e5a2a1e201d7f1a71310301128bcec5401697
Howard Chiam <hchiam@users.noreply.github.com>
2020-01-26 20:00:10 -0500

    ignore node_modules + tell user where .zip file is

--- zip_all_in_this_folder.sh
+++ zip_all_in_this_folder.sh
@@ -2,4 +2,8 @@
 
 # To run this file: bash zip_all_in_this_folder.sh
 
-zip -r compressed_folder.zip *
+zip -r compressed_folder.zip * -x "node_modules/*"
+
+echo
+echo "created compressed_folder.zip inside this folder"
+echo
