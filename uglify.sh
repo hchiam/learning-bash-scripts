425420f842394bebf08aef8667a084a91e906d14
Howard Chiam <hchiam@users.noreply.github.com>
2020-04-02 23:43:52 -0400

    npm install too

--- uglify.sh
+++ uglify.sh
@@ -1,3 +1,6 @@
+npm install uglify-es -g
+
 # You can set this in your .bash_profile:
 function ugly() { uglifyjs "$1" -o "$2" -m -c; }
+
 # now you can do type less and do something like this: ugly input.js output.js
