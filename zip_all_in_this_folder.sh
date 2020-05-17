e68d385997fc9d9f1222ae26a0d63450297f99dd
Howard Chiam <hchiam@users.noreply.github.com>
2020-05-17 13:24:12 -0400

    add global alias example (commented-out)

--- zip_all_in_this_folder.sh
+++ zip_all_in_this_folder.sh
@@ -4,6 +4,9 @@
 
 zip -r compressed_folder.zip * -x "node_modules/*"
 
+# or you could set up an alias to use globally:
+# alias zipup='zip -r ${PWD##*/}.zip * .eslintignore .eslintrc.js .gitignore .travis.yml -x "node_modules/*"; echo; echo "created ${PWD##*/}.zip inside this folder"; echo;'
+
 echo
 echo "created compressed_folder.zip inside this folder"
 echo
