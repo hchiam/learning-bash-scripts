0ce0fcba58a898b67d82548d209343f8a047e7e1
hchiam <hchiam@users.noreply.github.com>
2020-06-21 09:36:53 -0400

    update SRI script for less steps for the user

---
+++ FILENAME.js
@@ -0,0 +1,1 @@
+alert("This is just a test file to be used by a bash script!");
--- generate-hash-for-sri.sh
+++ generate-hash-for-sri.sh
@@ -1,5 +1,5 @@
-cat FILENAME.js | openssl dgst -sha384 -binary | openssl base64 -A
+echo sha384-$(cat FILENAME.js | openssl dgst -sha384 -binary | openssl base64 -A)
 # or
-shasum -b -a 384 FILENAME.js | awk '{ print $1 }' | xxd -r -p | base64
+echo sha384-$(shasum -b -a 384 FILENAME.js | awk '{ print $1 }' | xxd -r -p | base64)
 
-# (then append "sha384-" + the output to get the thing you'd put into the integrity attribute: https://github.com/hchiam/learning-sri)
+# (the output is the thing you'd put into the integrity attribute: https://github.com/hchiam/learning-sri)
