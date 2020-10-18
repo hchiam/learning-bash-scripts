55e4bc68ba86f73aa27febbdbc89c84653d839c5
hchiam <hchiam@users.noreply.github.com>
2020-10-18 15:30:40 -0400

    link to SO question with alteratives + pros + cons

--- prepend-text.sh
+++ prepend-text.sh
@@ -1,2 +1,5 @@
 cat <(echo "// A comment to prepend") script.js > temp.js
 mv temp.js script.js
+
+# There are a few other ways to prepend text, with pros and cons: 
+# https://stackoverflow.com/questions/10587615/unix-command-to-prepend-text-to-a-file
