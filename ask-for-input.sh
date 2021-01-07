207755d56a705fce330c488b8092b62e83ccd7cd
Howard Chiam <hchiam@users.noreply.github.com>
2021-01-06 23:37:24 -0500

    better example of bash prompt

--- ask-for-input.sh
+++ ask-for-input.sh
@@ -1,3 +1,16 @@
-echo "Type the input and hit enter:"
-read input
+# echo "Type the input and hit enter:"
+# read input
+# echo $input
+
+# or this:
+
+read -p "Please enter y or n: " input
 echo $input
+
+if [[ -z "$input" ]] || [[ $input == "y" ]]
+then
+  echo "You entered y (or nothing)"
+else
+  echo "You entered n"
+fi
+
