43cb20523e6d6efe8e88b769232fefa6b754689c
hchiam <hchiam@users.noreply.github.com>
2021-08-03 19:43:11 -0400

    notes

--- stop_port_8080.sh
+++ stop_port_8080.sh
@@ -1,1 +1,5 @@
+# https://stackoverflow.com/questions/11583562/how-to-kill-a-process-running-on-particular-port-in-linux
+
+# technically it actually kills any process listening to port 8080:
+
 kill $(lsof -t -i:8080)
