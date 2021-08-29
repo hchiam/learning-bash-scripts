d2ad2c16464b3a3a52aeafd6ca9de5a5167a0c39
hchiam <hchiam@users.noreply.github.com>
2021-08-29 10:43:06 -0400

    don't use -e option in package.json

--- echo-colours.sh
+++ echo-colours.sh
@@ -1,4 +1,4 @@
-# for example, in package.json:
-# "reminder": "echo; echo; echo -e \"don't forget to run \\033[1;34mbash get-integrity.sh\\033[0m\"; echo; echo;"
+# for example, in package.json: (don't use the -e option)
+# "reminder": "echo; echo; echo \"don't forget to run \\033[1;34mbash get-integrity.sh\\033[0m\"; echo; echo;"
 
 echo; echo; echo -e "don't forget to run \\033[1;34mbash get-integrity.sh\\033[0m"; echo; echo;
