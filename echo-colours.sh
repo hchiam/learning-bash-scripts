08019d380996eb58340b211a5681e9d454012e82
hchiam <hchiam@users.noreply.github.com>
2021-08-29 10:41:54 -0400

    echo-colours.sh

---
+++ echo-colours.sh
@@ -0,0 +1,4 @@
+# for example, in package.json:
+# "reminder": "echo; echo; echo -e \"don't forget to run \\033[1;34mbash get-integrity.sh\\033[0m\"; echo; echo;"
+
+echo; echo; echo -e "don't forget to run \\033[1;34mbash get-integrity.sh\\033[0m"; echo; echo;
