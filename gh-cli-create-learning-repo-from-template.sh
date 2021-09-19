f79847fe9af04baef9238289f3e8fff3d570e266
hchiam <hchiam@users.noreply.github.com>
2021-09-19 12:04:02 -0400

    gh-cli-create-learning-repo-from-template.sh

---
+++ gh-cli-create-learning-repo-from-template.sh
@@ -0,0 +1,9 @@
+# You need to install gh cli first.
+# https://github.com/hchiam/learning-gh
+
+function learn() {
+  echo
+  echo "Creating repo: learning-$1"
+  echo
+  gh repo create --template learning-template "learning-$1"
+}
