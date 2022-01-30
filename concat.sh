38611be29fea31b68957f154005d272bc4703208
hchiam <hchiam@users.noreply.github.com>
2022-01-30 10:54:19 -0500

    concat example with cat

---
+++ concat.sh
@@ -0,0 +1,5 @@
+# # append to concat_test_output.js:
+# cat concat_test_1.js concat_test_2.js >> concat_test_output.js
+
+# overwride concat_test_output.js:
+cat concat_test_1.js concat_test_2.js > concat_test_output.js
