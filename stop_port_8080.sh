34f945d1a98056f8943e471a63cb7e270560dab0
Howard Chiam <hchiam@users.noreply.github.com>
2022-06-28 22:50:10 -0600

    note kill-port

--- stop_port_8080.sh
+++ stop_port_8080.sh
@@ -3,3 +3,7 @@
 # technically it actually kills any process listening to port 8080:
 
 kill $(lsof -t -i:8080)
+
+# OR: https://www.npmjs.com/package/kill-port
+# yarn global add kill-port
+# kill-port 8080
