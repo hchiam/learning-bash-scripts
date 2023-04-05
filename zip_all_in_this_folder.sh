80b03bc5e42c2f26547c7f751b2bac2dcac5d016
Howard Chiam <hchiam@users.noreply.github.com>
2023-04-04 20:39:21 -0600

    Update zip_all_in_this_folder.sh

--- zip_all_in_this_folder.sh
+++ zip_all_in_this_folder.sh
@@ -2,10 +2,10 @@
 
 # To run this file: bash zip_all_in_this_folder.sh
 
-zip -r compressed_folder.zip * -x "node_modules/*"
+zip -r compressed_folder.zip * -x "node_modules/*" -x "*.zip"
 
 # or you could set up an alias to use globally:
-# alias zipup='zip -r ${PWD##*/}.zip * .eslintignore .eslintrc.js .gitignore .travis.yml *.zip -x "node_modules/*"; echo; echo "created ${PWD##*/}.zip inside this folder"; echo;'
+# alias zipup='zip -r ${PWD##*/}.zip * -x *.zip -x .husky/* -x node_modules/* -x ".github/*" -x .eslintignore -x .eslintrc.js -x .gitignore -x .travis.yml -x CODEOWNERS -x contributing.md; echo; echo "created ${PWD##*/}.zip inside this folder"; echo;'
 
 echo
 echo "created compressed_folder.zip inside this folder"
